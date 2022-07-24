576416de465d1f8bd47e5f90e61fb87ee3a0f467
ConradoClarke <conradoclarke@gmail.com>
2022-07-24 01:43:58 -0300

    Songs

---
+++ Assets/Scripts/ChangeSong.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Licht.Unity.Objects;
+using UnityEngine;
+
+public class ChangeSong : MonoBehaviour
+{
+    public AudioClip Clip;
+
+    private SongAudioSource _songAudioSource;
+
+    private void Awake()
+    {
+        _songAudioSource = SceneObject<SongAudioSource>.Instance();
+    }
+
+    private void OnEnable()
+    {
+        _songAudioSource.ChangeClip(Clip);
+    }
+}
---
+++ Assets/Scripts/SongAudioSource.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Licht.Impl.Orchestration;
+using Licht.Unity.Builders;
+using Licht.Unity.Objects;
+using UnityEngine;
+
+public class SongAudioSource : BaseUIObject
+{
+    public AudioSource AudioSource;
+
+    public void ChangeClip(AudioClip clip)
+    {
+        DefaultMachinery.AddBasicMachine(CrossFade(clip));
+    }
+
+    private IEnumerable<IEnumerable<Action>> CrossFade(AudioClip clip)
+    {
+        yield return new LerpBuilder(f => AudioSource.volume = f, () => AudioSource.volume)
+            .SetTarget(0)
+            .Over(1)
+            .UsingTimer(UITimer)
+            .Build();
+
+        AudioSource.clip = clip;
+        AudioSource.Play();
+
+        yield return new LerpBuilder(f => AudioSource.volume = f, () => AudioSource.volume)
+            .SetTarget(1)
+            .Over(0.25f)
+            .UsingTimer(UITimer)
+            .Build();
+    }
+}
